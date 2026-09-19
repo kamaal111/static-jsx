d1fa02a97f37de768d19801f5e37037f729c3f81
Kamaal Farah <kamaal.f1@gmail.com>
2026-09-20 01:23:50 +0200

    Fix pnpm in dev container

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,9 +7,10 @@ nvm alias default "$(cat .nvmrc)"
 
 export PNPM_HOME="$HOME/.local/share/pnpm"
 export PATH="$PNPM_HOME/bin:$PATH"
+PNPM_VERSION="$(jq -r '.devEngines.packageManager.version | match("[0-9]+").string' package.json)"
 curl -fsSL https://get.pnpm.io/install.sh | env \
   ENV="$HOME/.zshrc" \
-  PNPM_VERSION="$(jq -r '.devEngines.packageManager.version' package.json)" \
+  PNPM_VERSION="$PNPM_VERSION" \
   SHELL="$(command -v zsh)" \
   sh -
 
